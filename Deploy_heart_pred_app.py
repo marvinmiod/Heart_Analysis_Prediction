83eb11a8cdb1f378b783c0bb8b470845aae85376
Marvin <102019168+marvinmiod@users.noreply.github.com>
2022-05-17 16:54:11 +0800

    Add files via upload

---
+++ Deploy_heart_pred_app.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 14:52:17 2022
+
+Load and Deploy model
+
+@author: Marvin
+"""
+
+import re
+import pandas as pd
+import numpy as np
+import datetime
+import os
+import pickle
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+#%%
+
+PKL_NAME = os.path.join(os.getcwd(), 'best_model.pkl')
+
+# load saved model
+with open(PKL_NAME, 'rb') as file:
+    model = pickle.load(file)
+    
+# Load x_test, and y_test data before testing model
+#score = best_model.score(X_test, y_test)
+#print(score*100, '%')
+
+#If using Machine Learning load model
+#model = pickle.load(open(PKL_NAME))
+
+heart_attack_chance = {0: 'Negative', 1: 'Positive'}
+
+#%% Deployment
+
+# example of patient's info being added into prediction model,
+# 4 features selected, pregnancies (2), Glucose(183), age (32), BMI(35.1)
+# below is static input of 4 features
+#patient_info = np.array([2,183,32,35.1]) # test static input
+
+#must define the age,pregnancies,glucose,bmi variable first before can use code
+#patient_info = np.array([age,pregnancies,glucose,bmi])  
+#patient_info_scaled = minmax_scaler.transform(np.expand_dims(patient_info, 
+#                                                             axis=0))
+
+
+#outcome = model.predict(patient_info_scaled)
+#print(np.argmax(outcome))
+
+#print(diabetes_chance[np.argmax(outcome)])
+
+#%% Deployment
+
+# test static data:
+#patient_info = np.array([45,0,1,128,204,0,0,172,0,1.4,2,0,2])
+#patient_info_ex = np.expand_dims(patient_info, axis=0)                                                             
+
+#outcome = model.predict(patient_info_ex)
+#print(outcome)
+
+#print(heart_attack_chance[np.argmax(outcome)])
+
+
+#%% build app using streamlit
+
+#age','sex','cp','trtbps','chol','fbs','restecg','thalachh','exng','oldpeak',
+#'slp','caa','thall','output
+
+
+with st.form('Heart Attack Analysus Prediction Form'):
+    st.write("Patient's information")
+    # all features in the form
+    age = int(st.number_input('What is your age?'))
+    sex = int(st.number_input('Gender 0 (Male), 1(Female)'))
+    cp = int(st.number_input("""Chest Pain type\n 
+                  (0: typical angina), \n  
+                  (1: atypical angina), \n
+                  (2: non-anginal pain), \n
+                  (3: asymptomatic) """))
+    trtbps = int(st.number_input('Resting blood pressure (in mm Hg)'))
+    chol = int(st.number_input('Cholestoral in mg/dl'))
+    fbs = int(st.number_input('fasting blood sugar > 120 mg/dl(1 = true; 0 = false)'))
+    restecg = int(st.number_input("""Resting electrocardiographic results\n 
+                  (0: normal), \n  
+                  (1: having ST-T wave abnormality), \n
+                  (2: showing probable or definite left ventricular hypertrophy"""))
+    thalachh = int(st.number_input('Maximum heart rate achieved'))
+    exng = int(st.number_input('exercise induced angina (1 = yes; 0 = no)'))
+    oldpeak = int(st.number_input('oldpeak'))
+    slp = int(st.number_input('Slp? (0 = yes; 1 = no, 2 = maybe)'))
+    caa = int(st.number_input('number of major vessels (0-3)'))
+    thall = int(st.number_input('thall (1-3)'))
+    
+
+    
+    submitted = st.form_submit_button('Submit')
+    
+    if submitted == True:
+        # to take in user input for the 4 features, age, pregnancy, glucose and BMI
+        patient_info = np.array([age,sex,cp,trtbps])
+        patient_info_ex = np.expand_dims(patient_info, axis=0)
+       
+        outcome = model.predict(patient_info_ex)
+        #print out the outcome in the form using write function
+        st.write(heart_attack_chance[np.argmax(outcome)])
+            
+        #st.write([age,pregnancies,glucose,bmi])
+        #model.predict()
+        
+        if np.argmax(outcome)==1:
+            st.warning("""You have higher change to have heart attack, \n
+                       please exercise more and eat healthy diet""")
+        else:
+            st.balloons()
+            st.success('you have a healthy heart! keep up the exercise and healthy diet')
+    
+    st.write(submitted)
+
+# to deploy need to run the script below in the tf_env where the .py file is stored
+# streamlit run Deploy_model_diabetes.py
+# (tf_env) C:\<path of the saved deploy_heart_pred_app.py file>streamlit run Deploy_model_diabetes.py
+
+
+#%%
---
+++ training_heart.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 09:23:07 2022
+
+Heart attack predictor model
+
+This model predict the probability of one’s getting heart attack 
+can be determined by analysing the patient’s age, gender, exercise 
+induced angina, number of major vessels, 
+chest pain indication, resting blood pressure, cholesterol level, 
+fasting blood sugar, resting electrocardiographic results, 
+and maximum heart rate achieved.
+
+@author: Marvin Miod
+"""
+
+import re
+import pandas as pd
+import numpy as np
+import re
+import datetime
+import os
+import pickle
+import missingno as msno
+import math
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import Lasso
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+
+#----- to remove below
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import Sequential #model is only for Sequential Model
+from tensorflow.keras.layers import Dropout, Dense,  LSTM
+from tensorflow.keras.layers import Embedding, Bidirectional
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import BatchNormalization # to add after hidden layer
+from tensorflow.keras.utils import plot_model
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+#%%
+
+# save the model and save the log
+MODEL_SAVE_PATH = os.path.join(os.getcwd(), 'heart_attack_predictor.h5')
+# path where the logfile for tensorboard call back
+LOG_PATH = os.path.join(os.getcwd(),'Log_heart_attack')
+log_files = os.path.join(LOG_PATH, 
+                         datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
+DATASET = os.path.join(os.getcwd(), 'dataset', 'heart.csv')
+
+#PKL_NAME = os.path.join(os.getcwd(), 'best_model.pkl')
+PKL_NAME = 'best_model.pkl'
+
+#%%
+
+# EDA
+# Step 1) Load data
+df = pd.read_csv(DATASET)
+# create dummy data to work with
+dummy_df = df.copy()
+
+# Step 2) Data Inspection
+df.head()
+df.describe().T # check for outlier
+df.dtypes
+df.info()
+# visualise the data
+df.boxplot()
+# To check/visualise if there is any missing data
+df.isna().sum()
+
+# to visualize the missing numbers in the dataframe
+msno.matrix(df)
+msno.bar(df)
+
+column_names = ['age','sex','cp','trtbps','chol','fbs','restecg','thalachh','exng','oldpeak',
+'slp','caa','thall','output']
+
+data = pd.DataFrame(dummy_df)
+data.columns = column_names
+cor = data.corr()
+plt.figure(figsize=(12,10))
+sns.heatmap(cor,annot=True, cmap=plt.cm.Reds)
+plt.show() 
+
+# Step 3) Clean Data
+# Data cleaning not required because there is no NaN or missing values
+
+
+# Step 4) Features selection # this is a binary classification, outcome 0 or 1
+# based on the heatmap plot, the highest features that contribute 
+# to heart attack is cp, thalachh, and slp with value of 0.43, 0.42. 0.35 respectively
+
+# identify x features
+#x1 = dummy_df['cp'] # 0.43 
+#x2 = dummy_df['thalachh'] # 0.42
+#x3 = dummy_df['slp'] # 0.35
+#x = [x1,x2,x3]
+#x_features = np.array(x).T 
+
+# uncomment for all features
+x_features = dummy_df.drop(columns='output') 
+
+# target (y) is the output column
+y_target = dummy_df['output']
+
+#%% # Step 5) Data Pre-processing 
+# using ML pipeline to determine which scaling to use
+
+
+#%% Data 
+"""
+#identify x,y train and test data
+
+x_train,x_test,y_train,y_test = train_test_split(x_features, y_target, test_size=0.3,
+                                                 random_state=12)
+
+steps_LR = [('Linear Regression', LinearRegression())]
+steps_RD = [('Ridge', Ridge())]
+steps_tree = [('Tree', DecisionTreeClassifier())]
+steps_forest = [('Forest', RandomForestClassifier())]
+
+#load steps into pipeline
+# to load steps into pipeline
+pipeline_lr = Pipeline(steps_LR) 
+pipeline_rd = Pipeline(steps_RD) 
+pipeline_tree = Pipeline(steps_tree) 
+pipeline_forest = Pipeline(steps_forest) 
+
+pipelines = [pipeline_lr, pipeline_rd, pipeline_tree, pipeline_forest ] 
+
+for pipe in pipelines:
+    pipe.fit(x_train,y_train) 
+
+pipe_dict_model = { 0:'Linear Regression', 1: 'Ridge', 2: 'Tree', 
+                   3: 'Forest'} 
+
+"""    
+
+#%%# Machine Learning Pipeline
+
+# prepare x and y train and test data
+X_train,X_test, y_train,y_test = train_test_split(x_features, y_target, 
+                                                  test_size = 0.3, 
+                                                  random_state=12)
+
+# machine learning pipeline using standard scaler and PCA
+steps_tree = [('MinMax', StandardScaler()), 
+    ('PCA', PCA(n_components=2)),
+    ('Tree', DecisionTreeClassifier())]
+
+steps_forest = [('MinMax', StandardScaler()), 
+    ('PCA', PCA(n_components=2)),
+    ('Forest', RandomForestClassifier())]
+
+steps_logis = [('MinMax', StandardScaler()), 
+    ('PCA', PCA(n_components=2)),
+    ('Logic', LogisticRegression(solver='liblinear'))]
+
+steps_svc = [('MinMax', StandardScaler()), 
+    ('PCA', PCA(n_components=2)),
+    ('SVC', SVC())]
+
+steps_knn = [('MinMax', StandardScaler()), 
+    ('PCA', PCA(n_components=2)),
+    ('KNN', KNeighborsClassifier())]
+
+tree_pipeline = Pipeline(steps_tree)
+forest_pipeline = Pipeline(steps_forest)
+logic_pipeline = Pipeline(steps_logis)
+svc_pipeline = Pipeline(steps_svc)
+knn_pipeline = Pipeline(steps_knn)
+
+pipelines = [tree_pipeline, forest_pipeline, logic_pipeline, svc_pipeline,
+             knn_pipeline]
+
+for pipe in pipelines:
+    pipe.fit(X_train,y_train)
+    
+best_score = 0
+best_model = 0
+best_pipeline = ''
+    
+pipe_dict = {0:'Tree', 1:'Forest',2:'Logistic Regression', 3:'SVC', 4:'KNN'}
+print('Model Accuracy using Standard Scaler with PCA\n')
+
+
+# Model analysis
+for index, model in enumerate(pipelines):
+    #print(model.score(X_test,y_test))
+    print("{} Test Accuracy (%): {}".format(pipe_dict[index],model.score(X_test, 
+                                                                     y_test)*100))
+    if model.score(X_test,y_test) > best_score:
+        best_score = model.score(X_test,y_test)        
+        best_model = index
+        best_pipeline = model 
+        
+        
+print('\nBest score for Heart predictor model will be {} with % accuracy of {}' 
+      .format(pipe_dict[best_model],best_score*100 ))     
+
+# in this run the best model is Logistic Regression model with accuracy of 83%
+
+
+#%% Summary report
+
+#print the accuracy numbers in the console
+pred_x = model.predict(X_test)
+#y_true = np.argmax(y_test, axis=1)
+#y_pred = np.argmax(pred_x, axis=1)
+y_true = y_test
+y_pred = pred_x
+    
+cm = confusion_matrix(y_true, y_pred)
+cr = classification_report(y_true, y_pred)
+print(cr)
+    
+# Code for Confusion Matrix correlation graph
+#labels = [ '0', '1', '2' ,'3', '4', '5', '6' ,'7', '8', '9'] # manual way
+labels = [str(i) for i in range(10)] # using list comprehension
+#disp = ConfusionMatrixDisplay(confusion_matrix=cm, 
+#                              display_labels=np.unique(y_true))
+    
+# this one is to removed the display_labels with unique(y_true)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+
+#plot the quadrant
+disp.plot(cmap=plt.cm.Blues)
+plt.show() 
+
+
+#%% Save the best model (Logistic Regression model)
+
+#PKL_NAME = 'best_model.pkl'
+
+with open(PKL_NAME, 'wb') as file:
+    pickle.dump(best_pipeline, file)  
+
